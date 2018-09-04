e27b05d7868924a1cc852034bc3b123d19fbed45
Aleksandr Bezobchuk <alex@tendermint.com>
2018-09-04 09:16:19 -0400

    Update gov monitor to support codecs

--- monitor/gov.go
+++ monitor/gov.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/alexanderbez/titan/core"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/gov"
 )
 
 var (
@@ -18,6 +20,7 @@ const (
 )
 
 type baseGovMonitor struct {
+	codec  *wire.Codec
 	logger core.Logger
 	cm     *core.ClientManager
 	name   string
@@ -26,7 +29,17 @@ type baseGovMonitor struct {
 
 func newBaseGovMonitor(logger core.Logger, clients []string, name, memo string) *baseGovMonitor {
 	logger = logger.With("module", name)
-	return &baseGovMonitor{logger, core.NewClientManager(clients), name, memo}
+
+	codec := wire.NewCodec()
+	gov.RegisterWire(codec)
+
+	return &baseGovMonitor{
+		codec:  codec,
+		logger: logger,
+		cm:     core.NewClientManager(clients),
+		name:   name,
+		memo:   memo,
+	}
 }
 
 // Name implements the Monitor interface. It returns the monitor's name.
@@ -34,6 +47,28 @@ func (gm *baseGovMonitor) Name() string { return gm.name }
 
 // Memo implements the Monitor interface. It returns the monitor's memo.
 func (gm *baseGovMonitor) Memo() string { return gm.memo }
+
+func (gm baseGovMonitor) doRequest(url string) (res, id []byte, err error) {
+	rawBody, err := core.Request(url, core.RequestGET, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var proposals []gov.Proposal
+	err = gm.codec.UnmarshalJSON(rawBody, &proposals)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Return an empty response body and ID if no proposals were found so that no
+	// alert will be triggered.
+	if len(proposals) == 0 {
+		return nil, nil, nil
+	}
+
+	bodyHash := sha256.Sum256(rawBody)
+	return rawBody, bodyHash[:], nil
+}
 
 // GovProposalMonitor defines a monitor responsible for monitoring new
 // governance proposals.
@@ -51,9 +86,9 @@ func NewGovProposalMonitor(logger core.Logger, clients []string, name, memo stri
 // the SHA256 of the response body will be returned and an error otherwise.
 func (gpm *GovProposalMonitor) Exec(_ []string) (res, id []byte, err error) {
 	url := fmt.Sprintf("%s/gov/proposals?status=%s", gpm.cm.Next(), govProposalStatusNew)
-	gpm.logger.Debugf("monitoring for new governance proposals from: %s", url)
+	gpm.logger.Debug("monitoring for new governance proposals")
 
-	res, id, err = doGETRequest(url)
+	res, id, err = gpm.doRequest(url)
 	if err != nil {
 		gpm.logger.Errorf("failed to monitor new governance proposals; error: %v", err)
 		return nil, nil, err
@@ -79,28 +114,13 @@ func NewGovVotingMonitor(logger core.Logger, clients []string, name, memo string
 // error otherwise.
 func (gvm *GovVotingMonitor) Exec(_ []string) (res, id []byte, err error) {
 	url := fmt.Sprintf("%s/gov/proposals?status=%s", gvm.cm.Next(), govProposalStatusVoting)
-	gvm.logger.Debugf("monitoring for governance proposals in voting stage from: %s", url)
+	gvm.logger.Debug("monitoring for governance proposals in voting stage")
 
-	res, id, err = doGETRequest(url)
+	res, id, err = gvm.doRequest(url)
 	if err != nil {
 		gvm.logger.Errorf("failed to monitor governance proposals in voting stage; error: %v", err)
 		return nil, nil, err
 	}
 
 	return res, id, nil
-}
-
-func doGETRequest(url string) (res, id []byte, err error) {
-	rawBody, err := core.Request(url, core.RequestGET, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// validate if the response contains an empty JSON array
-	if string(rawBody) == "[]" {
-		rawBody = nil
-	}
-
-	bodyHash := sha256.Sum256(rawBody)
-	return rawBody, bodyHash[:], nil
 }
